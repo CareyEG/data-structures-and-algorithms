a66134be4037af9f99b8698cb9a98bef730ab426
Toshi <careyeg@gmail.com>
2019-06-23 00:52:42 -0700

    ch-7

--- code-challenges/6-12-challenges/challenges-07.test.js
+++ code-challenges/6-12-challenges/challenges-07.test.js
@@ -11,7 +11,11 @@ For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and
 ------------------------------------------------------------------------------------------------ */
 
 const forLoopTwoToThe = (arr) => {
-  // Solution code here...
+  let newArr = [];
+  for (let i = 0 ; i < arr.length; i++){
+  newArr.push(2 ** arr[i]);
+}
+return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -21,7 +25,12 @@ Write a function named forEachTwoToThe that produces the same output as your for
 ------------------------------------------------------------------------------------------------ */
 
 const forEachTwoToThe = (arr) => {
-  // Solution code here...
+  let newArr = [];
+  arr.forEach(element => {
+    newArr.push(2 ** element);
+  });
+  return newArr;
+
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -30,9 +39,7 @@ CHALLENGE 3
 Write a function named mapTwoToThe that produces the same output as your forLoopTwoToThe function from challenge 1 and your forEachTwoToThe function from challenge 2, but uses map instead of a for loop or forEach.
 ------------------------------------------------------------------------------------------------ */
 
-const mapTwoToThe = (arr) => {
-  // Solution code here...
-};
+const mapTwoToThe = (arr) => arr.map(arr => 2 ** arr)
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
@@ -45,7 +52,8 @@ For example: charCode(['h','i']) returns [104, 105].
 ------------------------------------------------------------------------------------------------ */
 
 const charCode = (arr) => {
-  // Solution code here...
+  let newArr = arr.map(element => element.charCodeAt());
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
